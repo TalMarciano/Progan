20d69e1c569813a2e0da1b315499b6e85a124fc3
TalMarciano <50293094+TalMarciano@users.noreply.github.com>
2019-05-05 19:27:40 +0300

    first commit - php files

---
+++ php/header.php
@@ -0,0 +1,13 @@
+<?php
+echo '<!-- Start Header Top Area -->
+    <div class="header-top-area">
+        <div class="container">
+            <div class="row">
+				<div class="logo-area">
+                    <img src="../images/logo.png" alt="" />
+				</div>
+            </div>
+        </div>
+    </div>
+    <!-- End Header Top Area -->'
+?>
