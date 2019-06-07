70585b5b5095971cf49c882bdfd9b9bd4ed88dae
TalMarciano <50293094+TalMarciano@users.noreply.github.com>
2019-06-07 08:35:23 +0300

    Update Products.php

--- php/Products.php
+++ php/Products.php
@@ -1,91 +1,130 @@
 <?php
-        require('init.php');
+    if (session_status() == PHP_SESSION_NONE) {
         session_start();
-        class Products {
-            private $productId;
-            private $productTypeId;
-            private $productName;
-            private $productDescription;
-            private $productQuantity;
-            private $productPrice;
-            private $productImages;
+    }
+    require('init.php');
+    class Products {
             public function __construct(){
-                $this->productImages = array();
             }
-            public function getId(){
-                return $this->productId;
-            }
-            public function getProductType(){
-                return $this->productTypeId;
-            }
-            public function getName(){
-                return $this->productName;
-            }
-            public function getDescription(){
-                return $this->productDescription;
-            }
-            public function getQuantity(){
-                return $this->productQuantity;
-            }
-            public function getPrice(){
-                return $this->productPrice;
-            }
-            public function getProductImages(){
-                return $this->productImages;
-            }
-            public static function getAllProductIdName(){
+            public function updateProductToRentInventory(){
                 global $db;
-                $sql = "select * from productTypeId";
-                $result = $db->query($sql);
-                $allTypes= null;
-                $i = 0;
-                if($result->num_rows>0){
-                    while($row = $result->fetch_assoc()){
-                        $allTypes[$i] = $row;
-                        $i += 1;
+                if(!$db->get_connection()){
+                    die("Connection failed!");
+                }
+                else{
+                    $productId = filter_input(INPUT_POST, 'productId');
+                    $vendor = filter_input(INPUT_POST, 'vendor');
+                    $productDescription = filter_input(INPUT_POST, 'productDescription');
+                    $unit = filter_input(INPUT_POST, 'unit');
+                    $priceDay = filter_input(INPUT_POST, 'priceDay');
+                    $productNameForUpdate = filter_input(INPUT_POST, 'productNameForUpdate');
+                    $sql = "update rentInventory set priceDay = " .$priceDay .", unit = " .$unit  .", productDescription ='" .$productDescription ."', vendor ='" .$vendor ."', productName ='" .$productNameForUpdate ."' 
+                    where productId = " .$productId;
+                    $result = $db->query($sql);
+                    if($result == 1){
+                        return 1;
                     }
-                    return $allTypes;
+                    else{
+                        return 0;
+                    }
+                }
+             }
+            public function deleteProductFromRentInvent(){
+                global $db;
+                if(!$db->get_connection()){
+                    die("Connection failed!");
+                }
+                else{
+                    $productId = filter_input(INPUT_POST, 'deleteProductFromRentInvent');
+                    $sql = "delete from rentInventory where productId =" .$productId;
+                    $result = $db->query($sql);
+                    if($result == 1){
+                        return 1;
+                    }
+                    else{
+                        return 0;
+                    }
                 }
                 
             }
-            private function setProductImages($id){
+            public function addNewProductToRentInventory(){
                 global $db;
-                $sql = "select * from productImages where productid = " .$id;
-                $result = $db->query($sql);
-                $i = 0;
-                while($row = $result->fetch_assoc()){
-                    $this->productImages[$i] = $row['location'];
-                    $i += 1;
+                $db->query("SET CHARACTER SET 'hebrew'");
+                $db->query("SET NAMES 'utf8'");
+                if(!$db->get_connection()){
+                    die("Connection failed!");
                 }
-            }
-            public static function getProductsByType($typeId){
-                global $db;
-                $sql = "select * from products where productTypeid=" .$typeId;
-                $resultSet = $db->query($sql);
-                $productsById = array();
-                $i = 0;
-                if($resultSet->num_rows > 0){
-                    while ($row = $resultSet->fetch_assoc()) {
-                        $p = new Products();
-                        $p->instantation($row);
-                        $p->setProductImages($p->getId());
-                        $productsById[$i] = $p;
-                        $i += 1;
+                else{
+                    $productId = filter_input(INPUT_POST, 'productId'); 
+                    $productName = filter_input(INPUT_POST, 'productName');
+                    $priceDay = filter_input(INPUT_POST, 'priceDay');
+                    $vendor = filter_input(INPUT_POST, 'vendor');
+                    $productDescription = filter_input(INPUT_POST, 'productDescription');
+                    $unit = filter_input(INPUT_POST, 'unit');
+                    $sql = "INSERT INTO rentInventory(productName, productId, vendor, productDescription, unit, priceDay) 
+                        values ('" .$productName ."'," .$productId .",'" .$vendor ."','" .$productDescription ."'," .$unit ."," .$priceDay .")";
+                    $result = $db->query($sql);
+                    if($result == 1){
+                        return 1;
                     }
-                }
-                return $productsById;           
-            }
-            private function hasAttribute($attribute){
-                $object_properties = get_object_vars($this);
-                return array_key_exists($attribute, $object_properties);
-            }
-            private function instantation($product_array){
-                foreach($product_array as $attribute=>$value){
-                    if($result = $this->hasAttribute($attribute)){
-                        $this->$attribute = $value;
+                    else{
+                        return 0;
                     }
                 }
             }
+            
+            public function deleteProductFromWix(){
+                $productId = filter_input(INPUT_POST, 'deleteProductId');
+                $urlForDeleteProductId = 'https://progangar.wixsite.com/mysite/_functions/removeProduct/' .$productId;
+                $result = file_get_contents($urlForDeleteProductId);
+                echo '<meta http-equiv="refresh" content="0">';
+            }
+            public function updateProductFromWix(){
+                $productIdForWix = filter_input(INPUT_POST, 'productIdForWixValue');
+                $productId = filter_input(INPUT_POST, 'productIdValue');
+                $shortDescription = filter_input(INPUT_POST, 'shortDescriptionValue');
+                $longDescription = filter_input(INPUT_POST, 'longDescriptionValue');
+                $regularPrice = filter_input(INPUT_POST, 'regularPriceValue');
+                $salePrice = filter_input(INPUT_POST, 'salePriceValue');
+                $images = filter_input(INPUT_POST, 'imagesValue');
+                $productName = filter_input(INPUT_POST, 'productNameValue');
+                
+                $urlForUpdateProduct = 'https://progangar.wixsite.com/mysite/_functions/updateProduct/' .$productIdForWix .'/' .$productId .'/' .$productName .'/' .$shortDescription .'/' .$longDescription .'/' .$regularPrice .'/' .$salePrice .'/' .$images;
+                $urlForUpdateProduct = str_replace(' ', '',$urlForUpdateProduct);
+                $result = file_get_contents($urlForUpdateProduct);
+                echo '<meta http-equiv="refresh" content="0">';
+
+             /*   $json = '{
+                "_id":"' .$productIdForWix .'",
+                "productId":"' .$productId.'",
+                "name":"' .$productName.'",
+                "shortDescription":"' .$shortDescription .'",
+                "description":"' .$longDescription.'",
+                "regularPrice":"' .$regularPrice.'",
+                "salePrice":"' .$salePrice.'",
+                "images":"' .$images .'"}';*/
+                
+               /* echo $json;
+               
+                $options = array(
+                      'http' => array(
+                        'method'  => 'POST',
+                        'header'  => 'Content-type: application/json', 
+                         'content' => $json
+                        )
+                    );
+                echo 'the options = ';    
+                var_dump($options);
+                
+                $context  = stream_context_create( $options );
+                $result = file_get_contents( $urlForDeleteProductId, false, $context );
+                echo 'the result = ' . $result;
+                $response = json_decode( $result );
+                var_dump($response);*/
+                
+            }
+            
+            
         }
         $products = new Products();
 ?>
