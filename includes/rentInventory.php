e9d8d7b1df25ac8b70e4b0c2c968093945c800b8
TalMarciano <50293094+TalMarciano@users.noreply.github.com>
2019-06-07 09:49:08 +0300

    Update rentInventory.php

--- includes/rentInventory.php
+++ includes/rentInventory.php
@@ -1,5 +1,14 @@
-<html>
+<!DOCTYPE html>
+<?php
+    if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+    }
+    if(!isset($_SESSION['userEmail'])){
+        header('Location: http://amitsl.mtacloud.co.il');
+    }
 
+?>
+<html>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -23,8 +32,6 @@
     <link rel="stylesheet" href="../css/animate.css">
     <!-- normalize CSS ============================================ -->
     <link rel="stylesheet" href="../css/normalize.css">
-    <!-- mCustomScrollbar CSS============================================ -->
-    <link rel="stylesheet" href="../css/scrollbar/jquery.mCustomScrollbar.min.css">
     <!-- jvectormap CSS	============================================ -->
     <link rel="stylesheet" href="../css/jvectormap/jquery-jvectormap-2.0.3.css">
     <!-- notika icon CSS ============================================ -->
@@ -39,6 +46,12 @@
     <link rel="stylesheet" href="../css/responsive.css">
     <!-- modernizr JS ============================================ -->
     <script src="../js/vendor/modernizr-2.8.3.min.js"></script>
+    <!-- Script for everyone ============================================ -->
+    <script src="../js/ourJs/scriptForEveryone.js"></script>
+    <script src="../js/ourJs/rentInventory.js"></script>
+
+   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script> 
+
 </head>
 
 <body>
@@ -65,42 +78,7 @@
         </div>
     </div>
     <!-- Breadcomb area End-->
-    
-        <!-- Search at Data Table Start-->
-<script>
-    function isExistingCellStartWith(children, length, filter) {
-        let td;
-         for (child = 1; child < length; child++) {  
-             td = children[child];
-             if(td.innerHTML.toUpperCase().includes(filter)) {
-                 return true
-             }
-        }
-        return false;
-    }
-
-    function myFunction() {
-      let input, filter, a, i, j, td, trs, child, table;
-      input = document.getElementById("mySearch");
-      filter = input.value.toUpperCase();
-      table = document.getElementById("data-table-basic");
-      trs = table.getElementsByTagName("tr");
-      for (i = 1; i < trs.length; i++) {
-            if(isExistingCellStartWith(trs[i].children, trs[i].children.length, filter)) {
-                trs[i].style.display = "";
-            } else {
-                trs[i].style.display = "none";
-            }
-        }
-    }
-</script>
-<!-- Search at Data Table End-->
-
-
-
-    
-    
-<!-- Data Table area Start-->
+    <!-- Data Table area Start-->
     <div class="data-table-area">
         <div class="container">
             <div class="row">
@@ -110,16 +88,128 @@
                             <h2 style="text-align:center">מלאי מוצרים להשכרות</h2>
                         </div>
                         <div class="table-responsive">
-                            <table id="data-table-basic" class="table table-striped">
-                                    
-        
-<!-- Button trigger modal -->
-<span class="buttonMargin" class= "textRight">
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-הוספת מוצר חדש
-</button>
-</span>
+                            <table dir = "rtl" id="data-table-basic" class="table table-striped">
+                            <!-- Button trigger modal -->
+                                <button type="button" style="float:left;" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                הוספת מוצר חדש  
+                                </button>
+                                <input type="text" id="mySearch" style ="float:right;" onchange="myFunction()" placeholder="חפש מוצר" title="Type in a category">
+                            <thead>
+                            <tr>
+                                <th class="centerTableTr">שם המוצר</th>
+                                <th class="centerTableTr">מק"ט</th>
+                                <th class="centerTableTr">שם הספק</th>
+                                <th class="centerTableTr">תיאור המוצר</th>
+                                <th class="centerTableTr">יח' זמינות להשכרה</th>
+                                <th class="centerTableTr">מחיר השכרה ליום</th>
+                                <th class="centerTableTr"></th>
+                                <th class="centerTableTr"></th>
 
+                            </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    require_once('../php/Database.php');
+                                    global $db;
+                                    $db->query("SET CHARACTER SET 'hebrew'");
+                                    $db->query("SET NAMES 'utf8'");
+                                    $result = $db ->query('select * from rentInventory');
+                                    //print_r($result);
+                                    //$result = $result->fetch_assoc();  //for testing
+                                    while($row = $result->fetch_assoc()){
+                                        echo '<tr>';
+                                            echo ' <td class="centerTableTr" id="productName' .$row['productId'] .'">' .$row['productName'] .'</td>';
+                                            echo ' <td class="centerTableTr" id="productId' .$row['productId'] .'">' .$row['productId'] .'</td>';
+                                            echo ' <td class="centerTableTr" id="vendor' .$row['productId'] .'">' .$row['vendor'] .'</td>';
+                                            echo ' <td class="centerTableTr" id="productDescription' .$row['productId'] .'">' .$row['productDescription'] .'</td>';
+                                            echo ' <td class="centerTableTr" id="unit' .$row['productId'] .'">' .$row['unit'] .'</td>';
+                                            echo '<td class="centerTableTr" id="priceDay' .$row['productId'] .'">' .$row['priceDay'] .'</td>';
+                                            echo '<td class="centerTableTr"><button type="button" class="buttonForManagers btn btn-primary waves-effect" id="updateProductInRentInventory' .$row['productId']  .'" onclick="updateProduct(' .$row['productId'] .')">ערוך</button></td>';   
+                                            echo '<td class="centerTableTr"><button type="button" class="buttonForManagers btn btn-primary waves-effect" id="deleteProductInRentInventory' .$row['productId']  .'" onclick="deleteProduct(' .$row['productId'] .')">מחק</button></td>';   
+                                        echo '</tr> ';
+                                        
+                                }
+                                echo '</tbody></table>';
+                                echo '<div id="isManagerParameterForThatUser" style="display:none;">' .$_SESSION['isManager'] .'</div>';
+                        		echo '<div id="hiddenFormForDeleteProductFromRentInvet" style="display:none;"></div>';
+                        		echo '<div id="hiddenFormForUpdateProductFromRentInvet" style="display:none;"></div>';
+                        		
+
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Data Table area End-->
+    <?php
+        require_once('../php/Products.php');
+        global $products;    
+        if(isset($_POST['productNameForUpdate'])){
+            $result = $products->updateProductToRentInventory();
+            if($result == 1){
+                echo '<meta http-equiv="refresh" content="0">';
+            }
+            else{
+                 echo "<script type='text/javascript'>
+                    Swal.fire({
+                      position: 'center',
+                      type: 'error',
+                      title: 'הייתה בעיה ',
+                      text: 'אנא נסה שנית או פנה לממונים',
+                      showConfirmButton: true,
+                      timer: 60000
+                    });
+                    </script>"; 
+            }
+        }
+        //////////this is for new product//////////////
+        if(isset($_POST['saveProduct'])){
+            $result = $products->addNewProductToRentInventory();
+            $isProductAdd = false;
+            echo $isProductAdd;
+            if($isProductAdd == 1){
+                echo '<meta http-equiv="refresh" content="0">';
+            }
+            else{ 
+                 echo "<script type='text/javascript'>
+                    Swal.fire({
+                      position: 'center',
+                      type: 'error',
+                      title: 'הייתה בעיה ',
+                      text: 'אנא נסה שנית או פנה לממונים',
+                      showConfirmButton: true,
+                      timer: 60000
+                    });
+                    </script>";   
+            }
+                
+        }
+        //////////this is for new product//////////////
+
+        if(isset($_POST['deleteProductFromRentInvent'])){
+            $result = $products->deleteProductFromRentInvent();
+            $isProductDeleted = false;
+            if($result == 1){
+                echo '<meta http-equiv="refresh" content="0">';
+            }
+            else{ 
+                 echo "<script type='text/javascript'>
+                    Swal.fire({
+                      position: 'center',
+                      type: 'error',
+                      title: 'הייתה בעיה ',
+                      text: 'אנא נסה שנית או פנה לממונים',
+                      showConfirmButton: true,
+                      timer: 60000
+                    });
+                    </script>";   
+            }
+        }
+        
+    ?>
+    
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
@@ -133,133 +223,25 @@
                 הוספת מוצר חדש
                 </h3>
           </div>
-          <div class="modal-body">
+          <div class="modal-body" style="text-align: right">
             <form method="POST" class="formRight">
             
-            <p> <input type="text" name="productName"> &nbsp; :שם מוצר </p>
-            <p> <input type="text" name="productId" > &nbsp; :מק"ט</p>
-            <p> <input type="text" name="vendor" > &nbsp; :ספק </p>
-            <p> <input type="text" name="productDescription" > &nbsp; :תיאור מוצר </p>
-            <p> <input type="text" name="unit" > &nbsp; :יח' זמינות להשכרה </p>
-            <p> <input type="text" name="priceD" > &nbsp; :מחיר השכרה ליום </p>
+            <p> <input type="text" name="productName" pattern= "[A-Za-z 0-9 א-ת]+" title="אנא הזן אותיות או ספרות בלבד" required > &nbsp; :שם מוצר </p>
+            <p> <input type="text" name="productId"  pattern= "[0-9]+" title="אנא הזן מספרים בלבד" required > &nbsp; :מק"ט</p>
+            <p> <input type="text" name="vendor"  pattern= "[A-Za-z א-ת]+" title="אנא הזן אותיות בלבד" required  > &nbsp; :ספק </p>
+            <p> <input type="text" name="productDescription"  pattern= "[A-Za-z 0-9 א-ת]+" title="אנא הזן אותיות או ספרות בלבד" required> &nbsp; :תיאור מוצר </p>
+            <p> <input type="text" name="unit"  pattern= "[0-9]+" title="אנא הזן מספרים בלבד" required  > &nbsp; :יח' זמינות להשכרה </p>
+            <p> <input type="text" name="priceDay"   pattern="[0-9]+" title="אנא הזן מספרים בלבד" required > &nbsp; :מחיר השכרה ליום </p>
 
             </div>
           <div class="modal-footer">
             <input type="button" value="סגור" data-dismiss="modal">
-            <input type="submit" value="שמור" name="register" >
+            <input type="submit" value="שמור" name="saveProduct" >
           </div>
         </form>
      </div>
       </div>
     </div>
-     
-    
-                                <thead>
-                                    <tr>
-                                        <th class="centerTableTr">מחיר השכרה ליום</th>
-                                        <th class="centerTableTr">יח' זמינות להשכרה</th>
-                                        <th class="centerTableTr">תיאור המוצר</th>
-                                        <th class="centerTableTr">שם הספק</th>
-                                        <th class="centerTableTr">מק"ט</th>
-                                        <th class="centerTableTr">שם המוצר</th>
-                                        <input type="text" id="mySearch" onchange="myFunction()" placeholder="חפש מוצר" title="Type in a category">
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <?php
-
-                                        require_once('../php/Database.php');
-                                        global $db;
-                                        $db->query("SET CHARACTER SET 'hebrew'");
-                                        $db->query("SET NAMES 'utf8'");
-                                        $result = $db ->query('select * from rentInventory');
-                                        
-                                        //print_r($result);
-                                        //$result = $result->fetch_assoc();  //for testing
-                                        
-                                        
-                                        while($row = $result->fetch_assoc()){
-                                            echo '<tr>';
-                                            echo '<td class="centerTableTr">' .$row['priceDay'] .'</td>';
-                                            echo ' <td class="centerTableTr">' .$row['unit'] .'</td>';
-                                            echo ' <td class="centerTableTr">' .$row['productDescription'] .'</td>';
-                                            echo ' <td class="centerTableTr">' .$row['vendor'] .'</td>';
-                                            echo ' <td class="centerTableTr">' .$row['productId'] .'</td>';
-                                            echo ' <td class="centerTableTr">' .$row['productName'] .'</td>';
-                                       echo '</tr> ';
-                                        }
-                                    ?>
-                                    </tr>
-                                   
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Data Table area End-->
-    
-    
-     
-          
-        <?php
-        require_once('../php/Database.php');
-    global $db;
-    $db->query("SET CHARACTER SET 'hebrew'");
-    $db->query("SET NAMES 'utf8'");
-		        if(!$db->get_connection()){
-                    die("Connection failed!");
-		        }
-		        else{
-               //     $rentId = filter_input(INPUT_POST, 'rentId'); 
-                    $productId = filter_input(INPUT_POST, 'productId'); 
-                    $productName = filter_input(INPUT_POST, 'productName');
-                    $priceD = filter_input(INPUT_POST, 'priceDay');
-                    $vendor = filter_input(INPUT_POST, 'vendor');
-                    $productDescription = filter_input(INPUT_POST, 'productDescription');
-                    $unit = filter_input(INPUT_POST, 'unit');
-
-
-           /*         if(User::checkUserName($email)){
-        		        echo "<script type='text/javascript'>Swal.fire('Failed to register', 'Email is invalid.', 'error')</script>";
-                    }
-          
-                    else if(User::checkNameAndLastName($firstName, $lastName)){
-        		        echo "<script type='text/javascript'>Swal.fire('Failed to register', 'Name or Last name is invalid.', 'error')</script>";
-                    }
-*/
-               //    else{
-                        $sql = "INSERT INTO rentInventory(productName, productId, vendor, productDescription, unit, priceDay) 
-                        values ('" .$productName ."','" .$productId ."', '".$vendor ."','" .$productDescription."',".$unit ."," .$priceDay .")";
-                        echo $sql; 
-                        $result = $db->query($sql);
-	                	Print_r ($result);
-                        if(!$result ){
-                            global $errorAddedUser;
-                            $errorAddedUser = TRUE;
-                            $invalidRegister = TRUE;
-                        }
-                        else{
-                            $invalidRegister = FALSE;
-                            global $userAddedSucessfuly;
-                            $userAddedSucessfuly = TRUE;
-                    }
-                }
-
-    
-    ?>
-    
-    
-    
-    
-    
-    
-    
-    
-    
     <!-- Start Footer area-->
     <div class="footer-copyright-area">
         <div class="container">
@@ -272,19 +254,13 @@
             </div>
         </div>
     </div>
-        <!-- End Footer area-->
-        
-
-        
-        
+    <!-- End Footer area-->
     <!-- jquery ============================================ -->
     <script src="../js/vendor/jquery-1.12.4.min.js"></script>
     <!-- bootstrap JS ============================================ -->
     <script src="../js/bootstrap.min.js"></script>
     <!-- wow JS ============================================ -->
     <script src="../js/wow.min.js"></script>
-    <!-- price-slider JS ============================================ -->
-    <script src="js/jquery-price-slider.js"></script>
     <!-- owl.carousel JS ============================================ -->
     <script src="../js/owl.carousel.min.js"></script>
     <!-- scrollUp JS ============================================ -->
@@ -295,8 +271,6 @@
     <script src="../js/counterup/jquery.counterup.min.js"></script>
     <script src="../js/counterup/waypoints.min.js"></script>
     <script src="../js/counterup/counterup-active.js"></script>
-    <!-- mCustomScrollbar JS ============================================ -->
-    <script src="js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
     <!-- jvectormap JS ============================================ -->
     <script src="../js/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
     <script src="../js/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
@@ -327,7 +301,6 @@
     <!-- plugins JS ============================================ -->
     <script src="../js/plugins.js"></script>
     <!-- main JS ============================================ -->
-    <script src="../js/main.js"></script>
 
 </body>
 
